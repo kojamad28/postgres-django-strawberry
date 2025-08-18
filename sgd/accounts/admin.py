eda9675d95645a4e5977ad83a03b6c98d381c304
kojamad28 <kojamad28@gmail.com>
2025-08-18 19:05:42 +0900

    updated the CustomUser model

--- sgd/accounts/admin.py
+++ sgd/accounts/admin.py
@@ -65,7 +65,7 @@ class CustomUserAdmin(UserAdmin):
     )
     list_display = ("username", "email")
     list_display_links = ("username",)
-    list_filter = ("is_admin",)
+    list_filter = ("is_staff",)
     search_fields = ("username", "email")
     ordering = ("username",)
     filter_horizontal = ()
